842cc7a34726ae61a6def5d0417063312b873ae9
DanieleTrn <danieletrn95@gmail.com>
2021-11-03 13:56:14 +0100

    nicknames added

--- Chat/Client.java
+++ Chat/Client.java
@@ -7,17 +7,25 @@ import java.util.logging.Logger;
 public class Client {
     public static void main(String[] args) throws IOException{
         Socket client = null;
+        String nickname = "";
         String msg = "";
         Scanner scan = new Scanner(System.in);
+        
+        //Scelgo un nickname
+        System.out.println("Scegli un nickname: \n");
+        nickname = scan.nextLine();
+        
+        //Tento la connessione
         client = new Socket("127.0.0.1", 6503);
         PrintStream os = new PrintStream(client.getOutputStream());
         while(true){
             msg = scan.nextLine();
-            os.println(msg);
+            os.println(nickname + ": " + msg);
             if(msg.equals("QUIT")){
                 break;
             }
         }
+	os.println(nickname + " ha abbandonato la chat");
         os.close();
     }
 }
